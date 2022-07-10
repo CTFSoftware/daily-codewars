7ee714ce4738bbb6f15744be33061305a8e52c5d
Caleb Flores <calebflores24@gmail.com>
2022-07-10 09:12:24 -0500

    Create Student's Final Grade.js

---
+++ 8kyu/Student's Final Grade.js
@@ -0,0 +1,8 @@
+//Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+function finalGrade (exam, projects) {
+  if(exam > 90 || projects > 10) return 100;
+  if(exam > 75 & projects >= 5) return 90;
+  if(exam > 50 & projects >= 2) return 75;
+  return 0;
+}
